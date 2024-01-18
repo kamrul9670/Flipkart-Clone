118bb26c93a59c10681a8bcd28697f56b5ed1377
kamrul9670 <kamrul9670@gmail.com>
2024-01-18 15:47:14 +0530

    building in process

---
+++ src/Components/header/Search.jsx
@@ -0,0 +1,43 @@
+
+
+import { InputBase ,Box  , styled} from '@mui/material';
+import React from 'react'
+  
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const SearchContainer = styled(Box)`
+background : #fff;
+width : 38%;
+border-radius : 3px;
+margin-left : 10px ;
+display : flex ;
+`
+
+const InputSearchBase = styled(InputBase)`
+  padding-left : 20px;
+  width : 100% ;
+`
+
+const SearchIconWrapper = styled(Box)`
+
+color : blue ;
+
+
+`
+
+ const Search = () => {
+  return (
+
+    <SearchContainer> 
+          <InputSearchBase 
+          placeholder='Search for products , brands and more '       />  
+             <SearchIconWrapper>
+                <SearchIcon />
+             </SearchIconWrapper>
+             </SearchContainer>
+        
+  )
+}
+
+export default Search;
